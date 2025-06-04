c0acbf1d6eefccfb849326cd4505539092ce0009
Noura Bouhssis <nora.bouhssis@gmail.com>
2025-06-04 11:18:58 +0100

    Update Button.tsx

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ButtonProps } from '@/interfaces';
+import { type ButtonProps } from '@/interfaces';
 
 
 const sizeStyles: Record<string, React.CSSProperties> = {
