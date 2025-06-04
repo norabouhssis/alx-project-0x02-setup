46b7c25d90171f686f356a051ea96b852a0a20cf
Noura Bouhssis <nora.bouhssis@gmail.com>
2025-06-04 11:24:43 +0100

    Update users.tsx

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,39 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import UserCard from '@/components/common/UserCard';
 import Header from '@/components/layout/Header';
 import { UserProps } from '@/interfaces';
 
-const Users = () => {
-  const [users, setUsers] = useState<UserProps[]>([]);
-  const [loading, setLoading] = useState(true);
+interface UsersProps {
+  users: UserProps[];
+}
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(data => {
-        setUsers(data);
-        setLoading(false);
-      });
-  }, []);
+const Users: React.FC<UsersProps> = ({ users }) => (
+  <>
+    <Header />
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Users</h1>
+      {users.map(user => (
+        <UserCard
+          key={user.id}
+          id={user.id}
+          name={user.name}
+          email={user.email}
+          address={user.address}
+        />
+      ))}
+    </div>
+  </>
+);
 
-  return (
-    <>
-      <Header />
-      <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-        <h1>Users</h1>
-        {loading && <p>Loading...</p>}
-        {users.map(user => (
-          <UserCard
-            key={user.id}
-            id={user.id}
-            name={user.name}
-            email={user.email}
-            address={user.address}
-          />
-        ))}
-      </div>
-    </>
-  );
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = await res.json();
+  return {
+    props: {
+      users,
+    },
+  };
 };
 
 export default Users;
